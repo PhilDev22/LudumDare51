1e5bc4f7155242e5eaa134b6480050638b366458
Thibaut Meyer <thibaut.meyer@posteo.eu>
2022-10-01 20:25:28 +0200

    makes idle animation consistent with last movement

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -7,6 +7,7 @@ onready var animated_sprite = $AnimatedSprite
 
 var velocity = Vector2()
 var direction
+var direction_last = Vector2(0,0)
 var collision_objects = []
 
 func _ready():
@@ -17,32 +18,32 @@ func _physics_process(delta):
 	
 	velocity = Vector2()
 	
-	var direction
 	if player_nr == 0:
 		direction = InputSystem.input_direction
 	elif player_nr == 1:
 		direction = InputSystem.input_direction_p2
 		
 	if direction:
+		direction_last = direction
 		print("moving player: ", player_nr)
-		
+
 		if direction.x == 1:
-			animated_sprite.set_animation("walk_horizontal") 
+			animated_sprite.set_animation("walk_horizontal")
 			animated_sprite.flip_h = false
 			velocity.x += 1
 		elif direction.x == -1:
-			animated_sprite.set_animation("walk_horizontal") 
+			animated_sprite.set_animation("walk_horizontal")
 			animated_sprite.flip_h = true
 			velocity.x -= 1
 		elif direction.y == 1:
-			animated_sprite.set_animation("walk_down") 
+			animated_sprite.set_animation("walk_down")
 			animated_sprite.flip_h = false
 			velocity.y += 1
 		elif direction.y == -1:
-			animated_sprite.set_animation("walk_up") 
+			animated_sprite.set_animation("walk_up")
 			animated_sprite.flip_h = false
 			velocity.y -= 1
-			
+
 	if velocity:
 		velocity = velocity.normalized() * speed
 		velocity = move_and_slide(velocity)
@@ -50,6 +51,18 @@ func _physics_process(delta):
 		for i in get_slide_count():
 			collision_objects.push_back(get_slide_collision(i))
 	else:
+		if direction_last.x == 1:
+			animated_sprite.set_animation("idle_horizontal")
+			animated_sprite.flip_h = false
+		elif direction_last.x == -1:
+			animated_sprite.set_animation("idle_horizontal")
+			animated_sprite.flip_h = true
+		elif direction_last.y == 1:
+			animated_sprite.set_animation("idle_down")
+			animated_sprite.flip_h = false
+		elif direction_last.y == -1:
+			animated_sprite.set_animation("idle_up")
+			animated_sprite.flip_h = false
 		animated_sprite.set_animation("idle")
 
 func _handle_colllisions():
