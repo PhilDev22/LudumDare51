b9bc7f9d32352e131e75e9196f584c01931b53cb
Phil <46622549+PhilDev22@users.noreply.github.com>
2022-10-01 20:45:29 +0200

    Added player collision detection

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
+class_name Player
+
 export (int) var player_nr = 0
 export (int) var speed = 200
+
+signal collision_with_player
 
 onready var animated_sprite = $AnimatedSprite
 
@@ -9,6 +13,9 @@ var velocity = Vector2()
 var direction
 var collision_objects = []
 
+func get_class():
+	return "Player"
+	
 func _ready():
 	animated_sprite.set_animation("idle") 
 	animated_sprite.set_playing(true)
@@ -24,7 +31,6 @@ func _physics_process(delta):
 		direction = InputSystem.input_direction_p2
 		
 	if direction:
-		print("moving player: ", player_nr)
 		
 		if direction.x == 1:
 			animated_sprite.set_animation("walk_horizontal") 
@@ -49,8 +55,15 @@ func _physics_process(delta):
 		collision_objects.clear()
 		for i in get_slide_count():
 			collision_objects.push_back(get_slide_collision(i))
+		if collision_objects:
+			_handle_collisions()
 	else:
 		animated_sprite.set_animation("idle")
 
-func _handle_colllisions():
-	return null
+
+func _handle_collisions():
+	
+	for i in collision_objects.size():
+		var collision_obj = collision_objects[i].collider
+		if collision_obj.get_class() == "Player":
+			emit_signal("collision_with_player")
--- Scripts/LevelLoader.gd
+++ Scripts/LevelLoader.gd
@@ -1,11 +1,22 @@
 extends Node2D
 export var player = "res://Scenes/Player/Player.tscn"
 
+var player0
+var player1
 
 func _ready():
-	instantiate_player(0)
-	instantiate_player(1)
-
+	start_game()
+	
+func start_game():
+	# TODO handle game start, set inital values, etc.
+	print("Game starts")
+	
+	# Instantiate player
+	player0 = instantiate_player(0)
+	player1 = instantiate_player(1)
+	player0.connect("collision_with_player", self, "_on_player_collision")
+	player1.connect("collision_with_player", self, "_on_player_collision")
+	
 
 func instantiate_player(player_nr = 0):
 	# This sets the player to appear at the correct area when loading into a new
@@ -25,3 +36,15 @@ func instantiate_player(player_nr = 0):
 	$InteractiveTerrain.add_child(player_spawn)
 	# Set player at the correct position (spawn point of zone)
 	player_spawn.position = spawn_points[index].position
+	
+	return player_spawn
+
+
+func _handle_game_over():
+	#TODO handle game over
+	print("Game Over!")
+	
+
+
+func _on_player_collision():
+	_handle_game_over()
