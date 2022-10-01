1625466a197a342f5f5ebd621a93027fc815afed
JohannaRahm <jojorahm@gmx.de>
2022-10-01 20:42:04 +0200

    add destroy function

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -7,6 +7,7 @@ onready var animated_sprite = $AnimatedSprite
 
 var velocity = Vector2()
 var direction
+var direction_destroy = Vector2(0, 1)
 var collision_objects = []
 
 func _ready():
@@ -52,5 +53,33 @@ func _physics_process(delta):
 	else:
 		animated_sprite.set_animation("idle")
 
+
 func _handle_colllisions():
 	return null
+
+
+func _process(delta):
+	if player_nr == 0:
+		if InputSystem.input_destroy:
+			if InputSystem.input_direction != Vector2(0, 0):
+				direction_destroy = InputSystem.input_direction
+			destroy(player_nr, direction_destroy)
+
+	if player_nr == 1:
+		if InputSystem.input_destroy_p2:
+			if InputSystem.input_direction_p2 != Vector2(0, 0):
+				direction_destroy = InputSystem.input_direction_p2
+			destroy(player_nr, direction_destroy)
+
+const destroy_path = preload("res://Scenes/Objects/Destroy.tscn")
+
+
+func destroy(var player_nr = 0, var direction_destroy = Vector2(0, 1)):
+	var destroy = destroy_path.instance()
+	get_parent().add_child(destroy)
+	destroy.position = $Node2D/Position2D.global_position
+	if player_nr == 0:
+		destroy.direction = direction_destroy
+	elif player_nr == 1:
+		destroy.direction = direction_destroy
+	print(destroy.velocity)
