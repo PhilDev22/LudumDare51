a0fef60970ca8913e8347b09ac638cd798098ce9
Phil <46622549+PhilDev22@users.noreply.github.com>
2022-10-02 12:47:32 +0200

    added range limit to bullets

--- Scripts/Destroy.gd
+++ Scripts/Destroy.gd
@@ -1,13 +1,21 @@
 extends KinematicBody2D
 
+const speed = 500
 
 var velocity = Vector2(0, 1)  # (1,0)-right, (0,1)-down, (0,-1)-up, ...
-var speed = 300
-
+var max_distance = GameData.tile_size * 2
+var current_distance =  Vector2(0, 0)
 
 func _physics_process(delta):
 	
-	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+	var move_step = velocity.normalized() * delta * speed
+	
+	# destroy self when max distance is reached
+	current_distance += move_step
+	if current_distance.x >= max_distance or current_distance.y >= max_distance:
+		queue_free()
+	
+	var collision_info = move_and_collide(move_step)
 	
 	if not collision_info: return
 	
